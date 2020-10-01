93a570510019c81da41221695d858f71a29cacd3
arkadiibasovych <arkadiybasovich@gmai.com>
2020-10-01 15:09:00 +0300

    добавляет код из коспекта

---
+++ JS/conspect.js
@@ -0,0 +1,39 @@
+// const string = "javascript";
+
+// for (const character of string) {
+//   console.log(character);
+// }
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// let total = 0;
+
+// for (let i = 0; i < matrix.length; i += 1) {
+//   console.log("Подмассив матрицы matrix[i]: ", matrix[i]);
+
+//   for (let j = 0; j < matrix[i].length; j += 1) {
+//     console.log("Элемент подмассива матрицы matrix[i][j]: ", matrix[i][j]);
+//     total += matrix[i][j];
+//   }
+// }
+
+// console.log(total);
+
+// const clients = ["Mango", "Ajax", "Poly"];
+// console.log(clients.unshift("Kiwi"));
+// console.log(clients);
+
+console.log(value); // undefined
+value = 5;
+
+if (true) {
+  console.log(value); // 5
+  var value = 10;
+  console.log(value); // 10
+}
+
+value = 15;
+console.log(value); // 15
